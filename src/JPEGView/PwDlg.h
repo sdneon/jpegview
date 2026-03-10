99144975e64225bae30000f8c148d60c50393faf
David Neon <sdneon@users.noreply.github.com>
2026-03-11 01:59:07 +0800

    cleanup

--- src/JPEGView/PwDlg.h
+++ src/JPEGView/PwDlg.h
@@ -16,17 +16,11 @@ public:
 		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
 		COMMAND_ID_HANDLER(IDOK, OnConfirmAndClose)
 		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
-		//COMMAND_HANDLER(IDC_RS_ED_PERCENTS, EN_CHANGE, OnPercentChanged)
-		//COMMAND_HANDLER(IDC_RS_ED_WIDTH, EN_CHANGE, OnWidthChanged)
-		//COMMAND_HANDLER(IDC_RS_ED_HEIGHT, EN_CHANGE, OnHeightChanged)
 	END_MSG_MAP()
 
 	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
 	LRESULT OnConfirmAndClose(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
-	//LRESULT OnPercentChanged(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
-	//LRESULT OnWidthChanged(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
-	//LRESULT OnHeightChanged(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 
 	CPwDlg(CString pw);
 	~CPwDlg(void);
@@ -37,10 +31,7 @@ public:
 private:
 	CButton m_btnOk;
 	CButton m_btnCancel;
-	//CStatic m_lblPercents;
 	CEdit m_edtPw;
 
 	CString m_pw;
-
-	void Close();
 };
